9e5a840aabf88d86fec2e34a7d170d504e3005f3
pga56 <parth_gulati@sfu.ca>
2024-08-25 12:32:01 -0700

    v0.0.5: Completed About section

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Computer Vision Engineer",
       icon: mobile,
     },
     {
-      title: "Backend Developer",
+      title: "Programming Enthusiast ",
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Average Gamer",
       icon: creator,
     },
   ];
