8fb45191bfd96f7774eec2b72a184ff8f6854b6c
pga56 <parth_gulati@sfu.ca>
2024-08-25 03:51:44 -0700

    v0.0.4: Started working on the about section; made the Hero section responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
-const About = () => {
+const ServiceCard = ({index, title, icon}) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full"></Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        I am a driven software developer and passionate about programming. I
+        enjoy solving complex problems and re-engineering inefficient processes.
+        I have experience in languages such as Python, Java, C++, HTML, CSS, and
+        JavaScript, and Web Development frameworks such as Node.js and React.js.
+        I also studied Computer Vision in-depth at SFU and have some projects
+        such as Facial Landmark Identification using PyTorch and Single-Shot
+        Multibox Object Detection.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 import * as THREE from "three";
 
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -21,8 +21,8 @@ const Computers = () => {
       <pointLight position={[0, 0, 0]} intensity={1} />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.5 :0.75}
+        position={isMobile? [0, -3, -1.5] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,15 +30,31 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(()=>{
+    const mediaQuery = window.matchMedia(
+      '(max-width: 500px)');
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+
+    return ()=> {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
   return (
-    <div className="w-full h-full absolute inset-0 top-[120px]">
+    <div className="w-full h-full">
       {/* Ensures full width and height for the canvas */}
       <Canvas
         frameloop="demand"
         shadows
         camera={{ position: [20, 3, 5], fov: 25 }}
         gl={{ preserveDrawingBuffer: true }}
-        className="w-full h-full"
       >
         <Suspense fallback={<CanvasLoader />}>
           <OrbitControls
@@ -51,7 +67,7 @@ const ComputersCanvas = () => {
               MIDDLE: THREE.MOUSE.NONE,
             }}
           />
-          <Computers />
+          <Computers isMobile={isMobile} />
         </Suspense>
       </Canvas>
     </div>
